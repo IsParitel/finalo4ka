6e27fa3594ca5d44f3f23a231d6ee22381d41761
Kanedgi <nechozahren@gmail.com>
2024-11-30 22:14:23 +0300

    UPD 0.16.0.2_4a
    Добавили SVOй дизайн глаVной страницы

--- client/src/pages/Main_page.js
+++ client/src/pages/Main_page.js
@@ -1,7 +1,13 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import {Button, Container, Row, Col, Card} from "react-bootstrap";
 import { Context } from "../index";
+import lupa from "../assets/lupa.png";
+import hat from "../assets/hat.png";
+import V1 from "../assets/V1.png";
+import V2 from "../assets/V2.png";
+import V3 from "../assets/V3.png";
+import V4 from "../assets/V4.png";
 import { REGISTER_ROUTE, JOB_LIST_ROUTE } from "../utils/consts";
 
 const MainPage = () => {
@@ -20,17 +26,217 @@ const MainPage = () => {
                 color: "white",
             }}
         >
-            {/* Верхняя часть с приветствием */}
-            <Row className="text-center mb-4">
+            {/* Верхняя часть с приветствием
+            <Row className="text-center mb-4" style={{ position: "relative", top: "-210px" }}>
                 <Col>
                     <h1 style={{ fontSize: "3rem", fontWeight: "bold", textShadow: "2px 2px 5px rgba(0,0,0,0.7)" }}>
-                        Добро пожаловать на платформу стажировок для студентов!
+                        Найди свою стажировку
                     </h1>
                     <p style={{ fontSize: "1.2rem", textShadow: "1px 1px 3px rgba(0,0,0,0.6)" }}>
                         Найдите стажировку своей мечты или добавьте свой профиль прямо сейчас.
                     </p>
                 </Col>
             </Row>
+            */}
+
+                <Row className="text-center mb-4" style={{position: "relative", top: "-210px"}}>
+                    <Col>
+                        <img
+                            src={lupa}
+                            alt="Overlay"
+                            style={{
+                                position: "absolute",
+                                top: "300",
+                                left: "70%",
+                                transform: "translate(-50%, -27%)",
+                                zIndex: "1",
+                                opacity: "0.5", // Прозрачность (опционально)
+                                width: "750px", // Ширина изображения (настройте по необходимости)
+                                height: "auto"
+                            }}
+                        />
+
+                        <img
+                            src={hat}
+                            alt="Overlay"
+                            style={{
+                                position: "absolute",
+                                top: "300",
+                                left: "70%",
+                                transform: "translate(-70%, -25%)",
+                                zIndex: "1",
+                                opacity: "0.05", // Прозрачность (опционально)
+                                width: "400px", // Ширина изображения (настройте по необходимости)
+                                height: "auto"
+                            }}
+                        />
+
+                        <img
+                            src={V1}
+                            alt="Overlay"
+                            style={{
+                                position: "absolute",
+                                top: "300",
+                                left: "70%",
+                                transform: "translate(-90%, 120%) rotate(320deg)",
+                                zIndex: "1",
+                                opacity: "0.05", // Прозрачность (опционально)
+                                width: "200px", // Ширина изображения (настройте по необходимости)
+                                height: "auto"
+                            }}
+                        />
+
+                        <img
+                            src={V2}
+                            alt="Overlay"
+                            style={{
+                                position: "absolute",
+                                top: "300",
+                                left: "70%",
+                                transform: "translate(-230%, 60%)",
+                                zIndex: "1",
+                                opacity: "0.05", // Прозрачность (опционально)
+                                width: "200px", // Ширина изображения (настройте по необходимости)
+                                height: "auto"
+                            }}
+                        />
+
+
+                        <img
+                            src={V3}
+                            alt="Overlay"
+                            style={{
+                                position: "absolute",
+                                top: "300",
+                                left: "70%",
+                                transform: "translate(-200%, -90%)",
+                                zIndex: "1",
+                                opacity: "0.05", // Прозрачность (опционально)
+                                width: "200px", // Ширина изображения (настройте по необходимости)
+                                height: "auto"
+                            }}
+                        />
+
+
+                        <img
+                            src={V4}
+                            alt="Overlay"
+                            style={{
+                                position: "absolute",
+                                top: "300",
+                                left: "70%",
+                                transform: "translate(30%, -50%)",
+                                zIndex: "1",
+                                opacity: "0.05", // Прозрачность (опционально)
+                                width: "200px", // Ширина изображения (настройте по необходимости)
+                                height: "auto"
+                            }}
+                        />
+
+
+                        <h1 style={{fontSize: "3rem", fontWeight: "bold", textShadow: "2px 2px 5px rgba(0,0,0,0.7)"}}>
+                            Найди свою стажировку
+                        </h1>
+                        <p style={{fontSize: "1.2rem", textShadow: "1px 1px 3px rgba(0,0,0,0.6)"}}>
+                            Найдите стажировку своей мечты или добавьте свой профиль прямо сейчас.
+                        </p>
+                    </Col>
+                </Row>
+
+            <Row>
+
+                <Card
+                    style={{
+                        transform: "translate(-470%, 410%)",
+                        position: 'absolute',
+                        top: '0',
+                        right: '0',
+                        width: '15%',
+                        height: '20%',
+                        background: 'rgba(255,255,255,0.68)',
+                        borderRadius: '15px',
+                        boxShadow: '0 4px 8px rgba(255,255,255,0.68)',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '10px',
+                        transition: 'all 0.3s ease',
+                    }}
+                >
+                    <div
+                        style={{
+                            fontSize: '14px',
+                            color: 'rgba(0,0,0,0.65)',
+                            textAlign: 'center',
+                        }}
+                    >
+                        Здесь будет дополнительная информация о вакансии.
+                    </div>
+                </Card>
+
+
+                <Card
+                    style={{
+                        transform: "translate(-280%, 410%)",
+                        position: 'absolute',
+                        top: '0',
+                        right: '0',
+                        width: '15%',
+                        height: '20%',
+                        background: 'rgba(255,255,255,0.68)',
+                        borderRadius: '15px',
+                        boxShadow: '0 4px 8px rgba(0,37,174,0.68)',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '10px',
+                        transition: 'all 0.3s ease',
+                    }}
+                >
+                    <div
+                        style={{
+                            fontSize: '14px',
+                            color: 'rgba(0,0,0,0.65)',
+                            textAlign: 'center',
+                        }}
+                    >
+                        Здесь будет дополнительная информация о вакансии.
+                    </div>
+                </Card>
+
+
+
+                <Card
+                    style={{
+                        transform: "translate(-90%, 410%)",
+                        position: 'absolute',
+                        top: '0',
+                        right: '0',
+                        width: '15%',
+                        height: '20%',
+                        background: 'rgba(255,255,255,0.68)',
+                        borderRadius: '15px',
+                        boxShadow: '0 4px 8px rgba(255,0,0,0.68)',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '10px',
+                        transition: 'all 0.1s ease',
+                    }}
+                >
+                    <div
+                        style={{
+                            fontSize: '14px',
+                            color: 'rgba(0,0,0,0.65)',
+                            textAlign: 'center',
+                        }}
+                    >
+                        Здесь будет дополнительная информация о вакансии.
+                    </div>
+                </Card>
+
+            </Row>
+
 
             {/* Кнопка */}
             <Row>
@@ -38,6 +244,7 @@ const MainPage = () => {
                     <Button
                         size="lg"
                         style={{
+                            transform: "translate(-0%, 150%)",
                             padding: "15px 30px",
                             fontSize: "1.2rem",
                             fontWeight: "bold",
@@ -46,17 +253,17 @@ const MainPage = () => {
                             color: "black",
                             border: "2px solid white",
                             boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.4)",
-                        }}
-                        onClick={() =>
-                            navigate(user.isAuth ? JOB_LIST_ROUTE : REGISTER_ROUTE)
-                        }
-                    >
-                        {user.isAuth ? "Просмотр вакансий" : "Регистрация"}
-                    </Button>
-                </Col>
-            </Row>
+                                }}
+                                onClick={() =>
+                                    navigate(user.isAuth ? JOB_LIST_ROUTE : REGISTER_ROUTE)
+                                }
+                            >
+                                {user.isAuth ? "Найти СВОю стажироVку" : "Регистрация"}
+                            </Button>
+                        </Col>
+                    </Row>
         </Container>
-    );
+);
 };
 
 export default MainPage;
