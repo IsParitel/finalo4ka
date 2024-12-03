24c98202fb66521ee3aa17ed4f31fa2771c9274c
IsParitel <isparitelvaper@yandex.ru>
2024-12-03 15:57:03 +0300

    Patch 0.17
    Добавлен .bat файл для запуска бэкенда и фронтэнд

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+rem Переход в первую папку и запуск команды
+cd /d "C:\Users\mishk\design\finalo4ka\server"
+start cmd /k "npm run dev"
+
+rem Переход во вторую папку и запуск команды
+cd /d "C:\Users\mishk\design\finalo4ka\client
+start cmd /k "npm start
+
+rem Завершение файла
+exit
