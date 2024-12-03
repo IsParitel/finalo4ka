bdead3ad2c0e6b9142361dee97b09deddb09531e
IsParitel <isparitelvaper@yandex.ru>
2024-12-03 16:08:28 +0300

    Patch 0.17.0.1
    исправлены пути в .bat файле

--- start.bat
+++ start.bat
@@ -1,11 +1,8 @@
 @echo off
-rem Переход в первую папку и запуск команды
-cd /d "C:\Users\mishk\design\finalo4ka\server"
+cd /d "%~dp0server"
 start cmd /k "npm run dev"
 
-rem Переход во вторую папку и запуск команды
-cd /d "C:\Users\mishk\design\finalo4ka\client
+cd /d "%~dp0client"
 start cmd /k "npm start
 
-rem Завершение файла
 exit
